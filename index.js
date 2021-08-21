bc5a4a60556f5326c8c235a878f2c0f514558531
klakegg <klakegg@users.noreply.github.com>
2021-08-21 14:26:21 +0000

    deploy: 7a4d72706354ebb9e96093132962e56b6bfa3978

---
+++ index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const core = require("@actions/core");
+const strftime = require("strftime");
+const { GITHUB_REF } = process.env;
+var timestamp = new Date(new Date().getTime() + (new Date().getTimezoneOffset() * 60 * 1000));
+var channel = process.env['default_channel'] || 'edge';
+var label = process.env['default_label'] || 'dev';
+var version = process.env['default_version'] || `dev-${strftime('%Y%m%d%H%M%Sz', timestamp)}`;
+if (GITHUB_REF.startsWith('refs/tags/v')) {
+    channel = GITHUB_REF.replace('refs/tags/', '').replace(/\.\d+/g, '');
+    version = GITHUB_REF.replace('refs/tags/v', '');
+    label = `v${version}`;
+}
+core.exportVariable('CHANNEL', channel);
+core.exportVariable('LABEL', label);
+core.exportVariable('VERSION', version);
