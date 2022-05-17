0d7f431cd41de95779ff4ce001f0cb8359d24248
Erlend Klakegg Bergheim <erlend@klakegg.net>
2022-05-17 20:54:59 +0200

    Adding test

--- src/ts/runner.test.ts
+++ src/ts/runner.test.ts
@@ -2,7 +2,7 @@ import { run } from './runner'
 
 test('Testing defaults', () => {
     var result = run({
-        GITHUB_REF: 'main',
+        GITHUB_REF: 'refs/heads/main',
     })
 
     expect(result['CHANNEL']).toBe('edge')
@@ -28,4 +28,19 @@ test('Return \'latest\' as channel', () => {
     expect(result['CHANNEL']).toBe('latest')
     expect(result['LABEL']).toBe('v1.2.3')
     expect(result['VERSION']).toBe('1.2.3')
+})
+
+test('Return \'latest\' as channel for custom configuration', () => {
+    var result = run({
+        GITHUB_REF: 'refs/tags/22.1.0-b1',
+        INPUT_CHANNEL_REPLACE: 'latest',
+        INPUT_CHANNEL_REGEX: '.*',
+        INPUT_MATCH: '^refs/tags/',
+        INPUT_VERSION_REGEX: '^refs/tags/(.+)$',
+        INPUT_LABEL_REGEX: '^refs/tags/([0-9]+\.[0-9]+)\..+',
+    })
+
+    expect(result['CHANNEL']).toBe('latest')
+    expect(result['LABEL']).toBe('22.1')
+    expect(result['VERSION']).toBe('22.1.0-b1')
 })
